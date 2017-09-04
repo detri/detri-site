1dd2783d7cc58fd69d5805b198075c297007465c
François Nguyen <lithos2406@live.fr>
2017-09-05 01:53:12 +0200

    moved the songs middleware to index.js

--- index.js
+++ index.js
@@ -7,6 +7,19 @@ const app = express();
 app.set('view engine', 'pug');
 
 app.use('/public', express.static('public'));
+
+app.use('/music/:song', (request, response, next) => {
+    let path = __dirname + '/songs/' + request.params.song + '.mp3';
+    path = unescape(path);
+    fs.access(path, (err) => {
+        if (err) {
+            console.log(err);
+            next();
+        }
+        response.sendFile(path);
+    });
+});
+
 app.use('/', router);
 
 app.listen(8080);
