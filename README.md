c060cbbc77477f00d40256765c955708dcbf1333
Aaron Dosser <adosser@kent.edu>
2018-03-04 23:42:10 -0500

    New redux branch, decided to redo the site

--- README.md
+++
@@ -1,2 +0,0 @@
-# detri-site
-A personal website and code playground.
--- src/app/footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class Footer extends React.Component {
-  render () {
-    return <footer>ded.zone&#8482; 2017</footer>;
-  }
-}
-
-export default Footer;
