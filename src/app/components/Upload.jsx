ff4c4c6ba53f884f61e8ff8aeb21b766f80ecaf7
Aaron Dosser <adosser@kent.edu>
2018-03-28 20:58:55 -0400

    Form proof of concept

--- src/app/components/Upload.jsx
+++ src/app/components/Upload.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import {
+  Hero,
+  HeroBody
+} from 'bloomer';
 import HeroBanner from './HeroBanner.jsx';
+import UploadForm from './UploadForm.jsx';
 
 const Upload = () => {
-  return (<HeroBanner
-    title='Upload a song'
-    isColor='primary'
-    subtitle='You know you want to.'
-  />);
+  return (
+    <React.Fragment>
+      <HeroBanner
+        title='Upload a song'
+        isColor='primary'
+        subtitle='Pick an mp3 from your computer.'
+      />
+      <Hero isColor='dark' isBold>
+        <HeroBody>
+          <UploadForm />
+        </HeroBody>
+      </Hero>
+    </React.Fragment>
+  );
 };
 
 export default Upload;
